25de5061ad1b53b03cf02ddd13deb3ce920e0545
technofab <admin@technofab.de>
2025-06-02 01:13:05 +0200

    fix(package): use filesets so nixtest doesnt get rebuilt all the time

--- package.nix
+++ package.nix
@@ -1,15 +1,21 @@
-{buildGoModule, ...}:
+{
+  lib,
+  buildGoModule,
+  ...
+}:
 buildGoModule {
   name = "nixtest";
   src =
     # filter everything except for cmd/ and go.mod, go.sum
-    builtins.filterSource (
-      path: type:
-        builtins.match ".*(/cmd/?.*|/go\.(mod|sum))$"
-        path
-        != null
-    )
-    ./.;
+    with lib.fileset;
+      toSource {
+        root = ./.;
+        fileset = unions [
+          ./cmd
+          ./go.mod
+          ./go.sum
+        ];
+      };
   subPackages = ["cmd/nixtest"];
   vendorHash = "sha256-Hmdtkp3UK/lveE2/U6FmKno38DxY+MMQlQuZFf1UBME=";
   meta.mainProgram = "nixtest";
