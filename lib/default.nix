c2ca17dfc514228b86a7d503c65c2e384d02b671
technofab <admin@technofab.de>
2025-06-02 01:13:54 +0200

    chore(lib): add assertion for script to not be null when type=="script"

--- lib/default.nix
+++ lib/default.nix
@@ -24,27 +24,28 @@
       if format == "json"
       then expected
       else lib.generators.toPretty {} expected;
-  in {
-    inherit type name description;
-    actual = actual';
-    expected = expected';
-    # discard string context, otherwise it's being built instantly which we don't want
-    actualDrv = builtins.unsafeDiscardStringContext (actualDrv.drvPath or "");
-    script =
-      if script != null
-      then
-        builtins.unsafeDiscardStringContext
-        (pkgs.writeShellScript "nixtest-${name}" ''
-          # show which line failed the test
-          set -x
-          ${script}
-        '').drvPath
-      else null;
-    pos =
-      if pos == null
-      then ""
-      else "${fileRelative}:${toString pos.line}";
-  };
+  in
+    assert lib.assertMsg (!(type == "script" && script == null)) "test ${name} has type 'script' but no script was passed"; {
+      inherit type name description;
+      actual = actual';
+      expected = expected';
+      # discard string context, otherwise it's being built instantly which we don't want
+      actualDrv = builtins.unsafeDiscardStringContext (actualDrv.drvPath or "");
+      script =
+        if script != null
+        then
+          builtins.unsafeDiscardStringContext
+          (pkgs.writeShellScript "nixtest-${name}" ''
+            # show which line failed the test
+            set -x
+            ${script}
+          '').drvPath
+        else null;
+      pos =
+        if pos == null
+        then ""
+        else "${fileRelative}:${toString pos.line}";
+    };
   mkSuite = name: tests: {
     inherit name tests;
   };
