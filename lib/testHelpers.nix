116f905b6c1df01f7c2dddef4f662abd0fcb5d42
technofab <admin@technofab.de>
2025-06-14 15:16:25 +0200

    chore(testHelpers): remove string context in toJsonFile

--- lib/testHelpers.nix
+++ lib/testHelpers.nix
@@ -2,6 +2,6 @@
   path = pkgs: "export PATH=${lib.makeBinPath pkgs}";
   pathAdd = pkgs: "export PATH=$PATH:${lib.makeBinPath pkgs}";
   scriptHelpers = builtins.readFile ./scriptHelpers.sh;
-  toJsonFile = any: builtins.toFile "actual" (builtins.toJSON any);
+  toJsonFile = any: builtins.toFile "actual" (builtins.unsafeDiscardStringContext (builtins.toJSON any));
   toPrettyFile = any: builtins.toFile "actual" (lib.generators.toPretty {} any);
 }
